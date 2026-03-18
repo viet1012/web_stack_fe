93696c36196a82a9aefed3bb1831fc82488ce988
Viet Ta <89030236+viet1012@users.noreply.github.com>
2026-03-18 17:02:39 +0700

    wip

--- lib/widgets/iframe_view.dart
+++ lib/widgets/iframe_view.dart
@@ -5,8 +5,13 @@ import 'dart:ui_web' as ui_web;
 
 class IframeView extends StatelessWidget {
   final String url;
+  final double scale; // Tỷ lệ zoom để nhìn full page
 
-  const IframeView({super.key, required this.url});
+  const IframeView({
+    super.key,
+    required this.url,
+    this.scale = 0.3, // Mặc định scale 30% để nhìn full page
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -14,14 +19,32 @@ class IframeView extends StatelessWidget {
 
     // ignore: undefined_prefixed_name
     ui_web.platformViewRegistry.registerViewFactory(viewId, (int viewId) {
+      // Tạo container wrapper
+      final container = html.DivElement()
+        ..style.width = '100%'
+        ..style.height = '100%'
+        ..style.overflow = 'hidden'
+        ..style.position = 'relative';
+
+      // Tạo iframe với transform scale
       final iframe = html.IFrameElement()
         ..src = url
         ..style.border = 'none'
         ..style.pointerEvents = 'none'
-        ..style.width = '100%'
-        ..style.height = '100%'
+        ..style.position = 'absolute'
+        ..style.top = '0'
+        ..style.left = '0'
+        ..style.transformOrigin = 'top left'
+        ..style.transform =
+            'scale($scale)' // Scale để nhìn full page
+        ..style.width =
+            '${100 / scale}%' // Tăng width tương ứng
+        ..style.height =
+            '${100 / scale}%' // Tăng height tương ứng
         ..allow = 'fullscreen';
-      return iframe;
+
+      container.children.add(iframe);
+      return container;
     });
 
     return HtmlElementView(viewType: viewId);
